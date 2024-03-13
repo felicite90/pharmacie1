adfc829db2991af66f9060bfbfa048dcee6d1957
felicity <bogueyerif@gmail.com>
2024-03-13 14:49:40 +0100

    ajout fonctionnalites

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+const express=require('express');
+
+const mongoose=require('mongoose');
+
+const app = require('./app');
+
+
+const PORT = 3001;
+const MONGO_URL = 'mongodb://localhost:27017';
+
+
+
+
+mongoose.connect(`${MONGO_URL}/pharmacieDB`).then(() => {
+    console.log('Connected to database '+ MONGO_URL)
+    app.listen(PORT, () => {
+        console.log('le serveur est lancé sur le port '+ PORT)
+    })
+}).catch(error => {
+    console.log(error);
+    throw new Error("Erreur de connexion à la base de donnée!");
+    
+})
